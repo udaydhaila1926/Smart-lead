d51a909f14b5cac42634a8dd1940cdd146e2fd7b
udaydhaila1926 <udaydhaila977@gmail.com>
2026-05-18 14:11:51 +0000

    verjson added

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+import API_URL from "../config/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
